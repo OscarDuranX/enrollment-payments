0d485ca946f7957bb13a0c3a2eb0ecaf92a92abb
Oscar Duran <xoscarduranx@gmail.com>
2016-11-22 16:35:22 +0100

    test add and travis

---
+++ tests/MisTestosTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Class MisTestos
+ */
+class MisTestosTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     *Mi prova tonta
+     * @test
+     * @group
+     */
+    public function provatontaTest()
+    {
+        // Preparació
+        // execució
+        // Assertion
+
+        $this-> assertIon;
+    }
+}
